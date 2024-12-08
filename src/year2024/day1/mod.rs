80da07efb6116f01baf1459bb25acb83de15099f
azerty29242 <94782485+azerty29242@users.noreply.github.com>
2024-12-08 10:25:21 +0100

    solve part 2 of day 1

--- src/year2024/day1/mod.rs
+++ src/year2024/day1/mod.rs
@@ -1,20 +1,35 @@
+use std::collections::HashMap;
+
 pub fn part1(input: String) -> String {
     let mut list1 = Vec::new();
     let mut list2 = Vec::new();
     for line in input.lines() {
         let mut numbers = line.split_whitespace();
-        list1.push(numbers.next().unwrap().parse::<i32>().unwrap());
-        list2.push(numbers.next().unwrap().parse::<i32>().unwrap());
-    }
+        list1.push(numbers.next().unwrap().parse::<u32>().unwrap());
+        list2.push(numbers.next().unwrap().parse::<u32>().unwrap());
+    };
     list1.sort();
     list2.sort();
-    let mut distances = Vec::new();
+    let mut distance_score: u32 = 0;
     for (first_number, second_number) in list1.iter().zip(list2.iter()) {
-        distances.push((first_number - second_number).abs());
-    }
-    format!("{}", distances.iter().sum::<i32>())
+        distance_score += std::cmp::max(first_number, second_number) - std::cmp::min(first_number, second_number);
+    };
+    format!("{}", distance_score)
 }
 
 pub fn part2(input: String) -> String {
-    input
+    let mut list1 = Vec::new();
+    let mut list2 = HashMap::new();
+    for line in input.lines() {
+        let mut numbers = line.split_whitespace();
+        list1.push(numbers.next().unwrap().parse::<u32>().unwrap());
+        let second_number = numbers.next().unwrap().parse::<u32>().unwrap();
+        *list2.entry(second_number).or_insert(0) += 1;
+    };
+    let mut similarity_score: u32 = 0;
+    for number in list1 {
+        let duplicates = list2.get(&number).unwrap_or(&0);
+        similarity_score += number * duplicates;
+    }
+    format!("{}", similarity_score)
 }
--- src/year2024/mod.rs
+++ src/year2024/mod.rs
@@ -1,19 +1,16 @@
 mod day1;
 
 pub fn solve(day: &str, input: String) {
-    println!("Solving part 1");
-
     let part1 = match day {
         "1" => Ok(day1::part1(input.clone())),
         _ => Err("Day {day} of year 2024 is not solved yet")
     }.unwrap();
-
-    println!("Solving part 2");
 
     let part2 = match day {
         "1" => Ok(day1::part2(input.clone())),
         _ => Err("Day {day} of year 2024 is not solved yet")
     }.unwrap();
 
-    println!("Here is the solution to part 1 : {part1}");
+    println!("Solution to part 1 : {}", part1);
+    println!("Solution to part 2 : {}", part2)
 }
